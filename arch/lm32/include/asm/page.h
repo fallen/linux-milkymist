9adf34e1c62ef283a3d2fcb906cdef6734738c60
Takeshi MATSUYA <macchan@sfc.wide.ad.jp>
2009-12-29 19:29:46 +0900

    redefined PAGE_SIZE MACRO

--- arch/lm32/include/asm/page.h
+++ arch/lm32/include/asm/page.h
@@ -1,10 +1,12 @@
 #ifndef _LM32_PAGE_H
 #define _LM32_PAGE_H
 
+#include <linux/const.h>
+
 /* PAGE_SHIFT determines the page size */
 
 #define PAGE_SHIFT	(12)
-#define PAGE_SIZE	(1UL << PAGE_SHIFT)
+#define PAGE_SIZE	(_AC(1,UL) << PAGE_SHIFT)
 #define PAGE_MASK	(~(PAGE_SIZE-1))
 
 #include <asm/setup.h>
