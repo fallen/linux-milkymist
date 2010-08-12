5f6f0e1da8470be8b864bbb570d78b9af5cd5661
Takeshi Matsuya <tmatsuya@tmatsuya-pc.(none)>
2010-08-12 23:00:20 +0900

    clean

--- arch/lm32/kernel/dma.c
+++ arch/lm32/kernel/dma.c
@@ -12,14 +12,3 @@
 #include <asm/cacheflush.h>
 
 struct dma_map_ops *dma_ops;
-
-#if 0
-dma_addr_t dma_map_single(struct device *dev, void *addr, size_t size,
-			  enum dma_data_direction dir)
-{
-	dma_addr_t handle = virt_to_phys(addr);
-	flush_dcache_range(handle, size);
-	return handle;
-}
-EXPORT_SYMBOL(dma_map_single);
-#endif
