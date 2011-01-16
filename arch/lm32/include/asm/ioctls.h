b8eaccee9171717dfc98b67a20ee445fe4c5d29e
Takeshi Matsuya <tmatsuya@macchan.sfc.wide.ad.jp>
2011-01-16 23:21:50 +0900

    LM32: defined TIOCGDEV

--- arch/lm32/include/asm/ioctls.h
+++ arch/lm32/include/asm/ioctls.h
@@ -53,6 +53,7 @@
 #define TIOCGSID	0x5429  /* Return the session ID of FD */
 #define TIOCGPTN	_IOR('T',0x30, unsigned int) /* Get Pty Number (of pty-mux device) */
 #define TIOCSPTLCK	_IOW('T',0x31, int)  /* Lock/unlock Pty */
+#define TIOCGDEV	_IOR('T',0x32, unsigned int) /* Get primary device node of /dev/console */
 #define TIOCSIG		_IOW('T',0x36, int)  /* Generate signal on Pty slave */
 
 #define FIONCLEX	0x5450  /* these numbers need to be adjusted. */
