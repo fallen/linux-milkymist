caa98eff6cb722297deb64589e6b7afa2fc1e08d
Lars-Peter Clausen <lars@metafoo.de>
2011-04-07 06:48:35 +0200

    lm32: ABI Cleanup
    Now that we can generate our own userland cleanup the ABI a bit more so we can
    reuse more from asm-generic.
    
    This will break existing userspace binaries.

--- arch/lm32/include/asm/ioctls.h
+++ arch/lm32/include/asm/ioctls.h
@@ -1,14 +1,1 @@
-/*
- * Based on:
- * include/asm-arm/ioctls.h
- */
-
-#ifndef _ASM_LM32_IOCTLS_H
-#define _ASM_LM32_IOCTLS_H
-
-/* TODO: This define can be dropped when the ABI is changed */
-#define FIOQSIZE	0x545E
-
 #include <asm-generic/ioctls.h>
-
-#endif
