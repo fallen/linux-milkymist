832b3f615ab9d39e2e2c3f12c636d642264dbcf2
Michael Walle <michael@walle.cc>
2011-07-25 23:49:33 +0200

    lm32: match against milkymist csr bus

--- arch/lm32/kernel/platform.c
+++ arch/lm32/kernel/platform.c
@@ -6,9 +6,15 @@
 #include <linux/kernel.h>
 #include <linux/of_platform.h>
 
+static const struct of_device_id lm32_of_bus_ids[] __initdata = {
+	{ .compatible = "simple-bus", },
+	{ .compatible = "milkymist,csr-bus", },
+	{}
+};
+
 static int __init lm32_device_probe(void)
 {
-	of_platform_populate(NULL, NULL, NULL);
+	of_platform_populate(NULL, lm32_of_bus_ids, NULL);
 	//of_platform_reset_gpio_probe();
 
 	return 0;
