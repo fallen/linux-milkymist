44520980ca017b05240837197b38128a27712a7b
Lars-Peter Clausen <lars@metafoo.de>
2011-07-26 13:51:19 +0200

    lm32: Speed up aligned memcpy

--- arch/lm32/lib/memcpy.c
+++ arch/lm32/lib/memcpy.c
@@ -1,62 +1,53 @@
 #include <linux/types.h>
 
-void * memcpy(void * to, const void * from, size_t n)
+void *memcpy_(void *to, const void *from, size_t n)
 {
-	// TODO: perhaps use this for more speed
-	/*
-  void *xto = to;
-  size_t temp;
+	const unsigned char *c_from = from;
+	unsigned char *c_to = to;
 
-  if (!n)
-    return xto;
-  if ((long) to & 1)
-    {
-      char *cto = to;
-      const char *cfrom = from;
-      *cto++ = *cfrom++;
-      to = cto;
-      from = cfrom;
-      n--;
-    }
-  if (n > 2 && (long) to & 2)
-    {
-      short *sto = to;
-      const short *sfrom = from;
-      *sto++ = *sfrom++;
-      to = sto;
-      from = sfrom;
-      n -= 2;
-    }
-  temp = n >> 2;
-  if (temp)
-    {
-      long *lto = to;
-      const long *lfrom = from;
-      for (; temp; temp--)
-	*lto++ = *lfrom++;
-      to = lto;
-      from = lfrom;
-    }
-  if (n & 2)
-    {
-      short *sto = to;
-      const short *sfrom = from;
-      *sto++ = *sfrom++;
-      to = sto;
-      from = sfrom;
-    }
-  if (n & 1)
-    {
-      char *cto = to;
-      const char *cfrom = from;
-      *cto = *cfrom;
-    }
-  return xto;
-#else
-	*/
-  const char *c_from = from;
-  char *c_to = to;
-  while (n-- > 0)
-    *c_to++ = *c_from++;
-  return((void *) to);
+	while (n--)
+		*c_to++ = *c_from++;
+	return((void *) to);
+}
+
+void *memcpy(void *to, const void *from, size_t n)
+{
+	unsigned long *l_to;
+	unsigned long *l_from;
+	const unsigned char *c_from = from;
+	unsigned char *c_to = to;
+	unsigned long to_align;
+	unsigned long from_align;
+
+	to_align = (unsigned long)to & 3;
+	from_align = (unsigned long)from & 3;
+
+	if (to_align == from_align && n >= 8 * 4) {
+		n -= to_align;
+		while (to_align--)
+			*c_to++ = *c_from++;
+
+		l_to = (unsigned long *)c_to;
+		l_from = (unsigned long *)c_from;
+
+		while (n >= 8 * 4) {
+			l_to[0] = l_from[0];
+			l_to[1] = l_from[1];
+			l_to[2] = l_from[2];
+			l_to[3] = l_from[3];
+			l_to[4] = l_from[4];
+			l_to[5] = l_from[5];
+			l_to[6] = l_from[6];
+			l_to[7] = l_from[7];
+			l_to += 8;
+			l_from += 8;
+			n -= 8 * 4;
+		}
+		c_to = (unsigned char *)l_to;
+		c_from = (unsigned char *)l_from;
+	}
+
+	while (n--)
+		*c_to++ = *c_from++;
+	return((void *) to);
 }
