2b719af43d284197a1b50c1f7ecf9e6da559a579
Lars-Peter Clausen <lars@metafoo.de>
2011-07-28 18:23:26 +0200

    lm32: Cleanup module loading

--- arch/lm32/kernel/module.c
+++ arch/lm32/kernel/module.c
@@ -5,157 +5,46 @@
 #include <linux/string.h>
 #include <linux/kernel.h>
 
-#if 0
-#define DEBUGP printk
-#else
-#define DEBUGP(fmt...)
-#endif
-
-void *module_alloc(unsigned long size)
+static int relocate_at(int rtype, unsigned* insn_addr, unsigned target_addr)
 {
-	if (size == 0)
-		return NULL;
-	return vmalloc(size);
-}
+	unsigned insn;
+	unsigned opcode;
 
+	switch (rtype) {
+	case R_LM32_32:
+		/* we can't really test whether the original data is an instruction
+			 that could be relocated, so we simply overwrite the data that's
+			 there */
+		//printk("R_LM32_32: overwriting 0x%08x with 0x%08x @ 0x%08x.\n", *insn_addr, target_addr, (unsigned)insn_addr);
+		*insn_addr = target_addr;
+		return 0; /* never fails */
 
-/* Free memory returned from module_alloc */
-void module_free(struct module *mod, void *module_region)
-{
-	vfree(module_region);
-	/* FIXME: If module_region == mod->init_region, trim exception
-           table entries. */
-}
+	case R_LM32_HI16:
+		/* the HI16 case is permitted on mvhi (orhi) instructions only! */
+		insn = *insn_addr;
+		if ((insn >> 26) == 0x1e) { /* the opcode for orhi is 6b'011110 (0x1e) */
+			insn &= 0xffff0000;
+			insn |= (target_addr >> 16);
+			*insn_addr = insn;
+			return 0;
+		}
+		//printk("insn word 0x%08x\n", insn);
+		break;
 
-/* We don't need anything special. */
-int module_frob_arch_sections(Elf_Ehdr *hdr,
-			      Elf_Shdr *sechdrs,
-			      char *secstrings,
-			      struct module *mod)
-{
-	return 0;
-}
+	case R_LM32_LO16:
+		insn = *insn_addr;
+		if ((insn >> 26) == 0x0e) { /* the opcode for ori is 6b'001110 (0x0e) */
+			insn &= 0xffff0000;
+			insn |= (target_addr & 0xffff);
+			*insn_addr = insn;
+			return 0;
+		}
+		break;
 
-static 
-int relocate_at(int rtype, unsigned* insn_addr, unsigned target_addr);
-
-int apply_relocate(Elf32_Shdr *sechdrs,
-		   const char *strtab,
-		   unsigned int symindex,
-		   unsigned int relsec,
-		   struct module *me)
-{
-	unsigned int i;
-	Elf32_Rel *rel = (void *)sechdrs[relsec].sh_addr;
-	Elf32_Sym *sym;
-	uint32_t *location;
-
-	DEBUGP("Applying relocate section %u to %u\n", relsec,
-	       sechdrs[relsec].sh_info);
-	for (i = 0; i < sechdrs[relsec].sh_size / sizeof(*rel); i++) {
-		/* This is where to make the change */
-		location = (void *)sechdrs[sechdrs[relsec].sh_info].sh_addr
-			+ rel[i].r_offset;
-		/* This is the symbol it is referring to.  Note that all
-		   undefined symbols have been resolved.  */
-		sym = (Elf32_Sym *)sechdrs[symindex].sh_addr
-			+ ELF32_R_SYM(rel[i].r_info);
-
-		if( -1 == relocate_at(
-					ELF32_R_TYPE(rel[i].r_info),
-					location,
-					sym->st_value) )
-			return -ENOEXEC;
-	}
-	return 0;
-}
-
-int apply_relocate_add(Elf32_Shdr *sechdrs,
-		       const char *strtab,
-		       unsigned int symindex,
-		       unsigned int relsec,
-		       struct module *me)
-{
-	unsigned int i;
-	Elf32_Rela *rel = (void *)sechdrs[relsec].sh_addr;
-	Elf32_Sym *sym;
-	uint32_t *location;
-
-	DEBUGP("Applying relocate_add section %u to %u\n", relsec,
-	       sechdrs[relsec].sh_info);
-	for (i = 0; i < sechdrs[relsec].sh_size / sizeof(*rel); i++) {
-		/* This is where to make the change */
-		location = (void *)sechdrs[sechdrs[relsec].sh_info].sh_addr
-			+ rel[i].r_offset;
-		/* This is the symbol it is referring to.  Note that all
-		   undefined symbols have been resolved.  */
-		sym = (Elf32_Sym *)sechdrs[symindex].sh_addr
-			+ ELF32_R_SYM(rel[i].r_info);
-		DEBUGP("relocation at offset %lx: symbol %lx value %lx addend %lx\n",
-				rel[i].r_offset, ELF32_R_SYM(rel[i].r_info),
-				sym->st_value, rel[i].r_addend);
-
-		if( -1 == relocate_at(
-					ELF32_R_TYPE(rel[i].r_info),
-					location,
-					sym->st_value + rel[i].r_addend) )
-			return -ENOEXEC;
-	}
-	return 0;
-}
-
-int module_finalize(const Elf_Ehdr *hdr,
-		    const Elf_Shdr *sechdrs,
-		    struct module *me)
-{
-	return 0;
-}
-
-void module_arch_cleanup(struct module *mod)
-{
-}
-
-static 
-int relocate_at(int rtype, unsigned* insn_addr, unsigned target_addr)
-{
-  unsigned insn;
-  unsigned opcode;
-
-  switch (rtype) {
-  case R_LM32_32:
-    /* we can't really test whether the original data is an instruction
-       that could be relocated, so we simply overwrite the data that's
-       there */
-    //printk("R_LM32_32: overwriting 0x%08x with 0x%08x @ 0x%08x.\n", *insn_addr, target_addr, (unsigned)insn_addr);
-    *insn_addr = target_addr;
-    return 0; /* never fails */
-
-  case R_LM32_HI16:
-    /* the HI16 case is permitted on mvhi (orhi) instructions only! */
-    insn = *insn_addr;
-    if ((insn >> 26) == 0x1e) { /* the opcode for orhi is 6b'011110 (0x1e) */
-      insn &= 0xffff0000;
-      insn |= (target_addr >> 16);
-      *insn_addr = insn;
-      return 0;
-    }
-    //printk("insn word 0x%08x\n", insn);
-    break;
-
-  case R_LM32_LO16:
-    insn = *insn_addr;
-    if ((insn >> 26) == 0x0e) { /* the opcode for ori is 6b'001110 (0x0e) */
-      insn &= 0xffff0000;
-      insn |= (target_addr & 0xffff);
-      *insn_addr = insn;
-      return 0;
-    }
-    break;
-
-  case R_LM32_CALL:
-    insn = *insn_addr;
-    opcode = insn >> 26;
-    if( (opcode == 0x3e /* calli */) )
+	case R_LM32_CALL:
+		insn = *insn_addr;
+		opcode = insn >> 26;
+		if (opcode == 0x3e /* calli */)
 		{
 			/* install a calli to the given target_addr relative from the instruction
 			 * address */
@@ -163,14 +52,58 @@ int relocate_at(int rtype, unsigned* insn_addr, unsigned target_addr)
 			imm26 = (imm26 >> 2) & 0x03FFFFFF;
 			insn = 0xF8000000 + imm26;
 			*insn_addr = insn;
-      return 0;
-    }
-    break;
+			return 0;
+		}
+		break;
 
-  default:
-    printk("ignoring relocation type %d @ 0x%08x\n", rtype, *insn_addr);
-  }
+	default:
+		printk("ignoring relocation type %d @ 0x%08x\n", rtype, *insn_addr);
+	}
 
-  return -1;
+	return -ENOEXEC;
 }
 
+static int _apply_relocate(Elf32_Shdr *sechdrs, const char *strtab,
+	unsigned int symindex, unsigned int relsec, struct module *me,
+	bool add)
+{
+	unsigned int i;
+	unsigned int target_addr;
+	Elf32_Rel *rel = (void *)sechdrs[relsec].sh_addr;
+	Elf32_Sym *sym;
+	uint32_t *location;
+	int ret;
+
+	pr_dbg("Applying relocate section %u to %u\n", relsec,
+				 sechdrs[relsec].sh_info);
+	for (i = 0; i < sechdrs[relsec].sh_size / sizeof(*rel); i++) {
+		/* This is where to make the change */
+		location = (void *)sechdrs[sechdrs[relsec].sh_info].sh_addr
+			+ rel[i].r_offset;
+		/* This is the symbol it is referring to.	Note that all
+			 undefined symbols have been resolved.	*/
+		sym = (Elf32_Sym *)sechdrs[symindex].sh_addr +
+			ELF32_R_SYM(rel[i].r_info);
+
+		target_addr = sym->st_value;
+		if (add)
+			target_addr += rel[i].r_addend;
+
+		ret = relocate_at(ELF32_R_TYPE(rel[i].r_info), location,
+				sym->st_value);
+		if (ret)
+			return ret;
+	}
+	return 0;
+
+int apply_relocate(Elf32_Shdr *sechdrs, const char *strtab,
+	unsigned int symindex, unsigned int relsec, struct module *me)
+{
+	return _apply_relocate(sechdrs, strtab, symindex, relsec, me, false);
+}
+
+int apply_relocate_add(Elf32_Shdr *sechdrs, const char *strtab,
+	unsigned int symindex, unsigned int relsec, struct module *me)
+{
+	return _apply_relocate(sechdrs, strtab, symindex, relsec, me, true);
+}
